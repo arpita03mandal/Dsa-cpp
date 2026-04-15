32640d06440608c31fa4656ba07feeed968ab7b9
Arpita Mandal <arpita03mandal@gmail.com>
2026-04-16 00:05:26 +0530

    Create print_all_elements.cpp

---
+++ print_all_elements.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int marks[5]={10,20,30,40,50};
+    int n=5;
+    for(int i=0;i<n;i++) {
+        cout<<marks[i]<<endl;
+    }
+    return 0;
+}
