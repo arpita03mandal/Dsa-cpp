06408455597209a5d3f2fbec867597a2e75d4686
Arpita Mandal <arpita03mandal@gmail.com>
2026-04-17 01:06:35 +0530

    Create VectorFrontBack.cpp

---
+++ VectorFrontBack.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+    vector<int>vec={1,2,3,4,5};
+    cout<<vec.front()<<endl;
+    cout<<vec.back()<<endl;
+    return 0;
+}
